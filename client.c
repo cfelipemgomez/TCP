f3fb10ecba02cd4ff5bb80ab052a2eb8ed2a7a76
Juan Fernando Echavarria Zapata <fernando.echavarria.ies@gmail.com>
2019-11-06 18:37:57 -0500

    feat: Se devuelven cambios hasta obtener la version en la que se logra establecer comunicacion pero de manera no exitosa, pues recibe los datos pero al no reconocerlos como codificados con certificado ssl, los rechaza.

--- client.c
+++ client.c
@@ -1,8 +1,3 @@
-//#include <openssl/applink.c>
-#include <openssl/bio.h>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
-//////////////////
 #include <netdb.h> 
 #include <stdio.h> 
 #include <stdlib.h> 
@@ -11,27 +6,7 @@
 #define MAX 300 
 #define PORT 1099 
 #define SA struct sockaddr 
-
-void InitializeSSL()
-{
-    SSL_load_error_strings();
-    SSL_library_init();
-    OpenSSL_add_all_algorithms();
-}
-
-void DestroySSL()
-{
-    ERR_free_strings();
-    EVP_cleanup();
-}
-
-void ShutdownSSL(SSL *cSSL)
-{
-    SSL_shutdown(cSSL);
-    SSL_free(cSSL);
-}
-
-void func(int sockfd, SSL *cSSL) 
+void func(int sockfd) 
 { 
 	char buff[MAX]; 
 	int n; 
@@ -40,9 +15,7 @@ void func(int sockfd, SSL *cSSL)
 		printf("Enter data to send : \n\t"); 
 		n = 0; 
 		while ((buff[n++] = getchar()) != '\n'); 
-		SSL_write(cSSL, buff, sizeof(buff)); 
-		printf("Se hizo el envío");
-		//write(sockfd, buff, sizeof(buff)); 
+		write(sockfd, buff, sizeof(buff)); 
 
 		bzero(buff, sizeof(buff)); 
 		read(sockfd, buff, sizeof(buff)); 
@@ -59,15 +32,8 @@ void func(int sockfd, SSL *cSSL)
 
 int main() 
 { 
-//int sockfd, newsockfd;
-SSL_CTX *sslctx;
-SSL *cSSL;
-
-
 	int sockfd, connfd; 
 	struct sockaddr_in servaddr, cli; 
-
-InitializeSSL();
 
 	// socket create and varification 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
@@ -92,16 +58,8 @@ InitializeSSL();
 	else
 		printf("connected to the server..\n"); 
 
-sslctx = SSL_CTX_new( SSLv23_server_method());
-SSL_CTX_set_options(sslctx, SSL_OP_SINGLE_DH_USE);
-int use_cert = SSL_CTX_use_certificate_file(sslctx, "cert.pem" , SSL_FILETYPE_PEM);
-
-int use_prv = SSL_CTX_use_PrivateKey_file(sslctx, "key.pem", SSL_FILETYPE_PEM);
-
-cSSL = SSL_new(sslctx);
-SSL_set_fd(cSSL, sockfd );
 	// function for chat 
-	func(sockfd, cSSL); 
+	func(sockfd); 
 
 	// close the socket 
 	close(sockfd); 
